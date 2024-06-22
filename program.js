c7c606ce5bd53be7b69ab657481a27b2d3298984
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2024-06-22 17:18:36 -0430

    Create string function "getComputerChoice"
    Gets a completely random choice for the computer in the game

--- program.js
+++ program.js
@@ -1,1 +1,32 @@
-console.log('hello')
+// Create function to get a random choice
+function getComputerChoice() {
+
+//      Get a random number from one through three and store it in a variable called choice
+        let choice = Math.floor(Math.random() * 3) + 1;
+
+//      If the number stored in choice is equal to 1:
+        if (choice === 1) {
+
+//          Set choice variable to "Rock" and return it
+            choice = "Rock";
+            return choice;            
+        }
+
+//      If the number stored in choice is equal to 2:
+        if (choice === 2) {
+
+//          Set choice variable to "Paper" and return it
+            choice = "Paper";
+            return choice;
+        }
+
+//      Otherwise:
+        else {
+
+//          Set choice variable to "Scissors" and return it
+            choice = "Scissors";
+            return choice;
+        }
+}
+
+console.log(getComputerChoice())
